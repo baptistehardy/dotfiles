64cbe0586f11eb410e298deaeaab30dfedb29000
Baptiste Hardy <baptiste@hardy.sh>
2024-08-14 23:01:33 +0200

    Some cleaning (fonts, XDG config...)

--- home/default.nix
+++ home/default.nix
@@ -22,6 +22,8 @@
     stateVersion = "24.05";
   };
 
+  xdg.enable = true;
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
--- modules/system.nix
+++ modules/system.nix
@@ -66,30 +66,11 @@ in {
 
   fonts = {
     packages = with pkgs; [
-      # icon fonts
-      material-design-icons
-
-      # normal fonts
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-
-      # nerdfonts
       (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
     ];
 
     # use fonts specified by user rather than default ones
     enableDefaultPackages = false;
-
-    # user defined fonts
-    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
-    # B&W emojis that would sometimes show instead of some Color emojis
-    fontconfig.defaultFonts = {
-      serif = ["Noto Serif" "Noto Color Emoji"];
-      sansSerif = ["Noto Sans" "Noto Color Emoji"];
-      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
-      emoji = ["Noto Color Emoji"];
-    };
   };
 
   programs.dconf.enable = true;
@@ -136,7 +117,6 @@ in {
     unzip
     curl
     git
-    htop
   ];
 
   # Enable sound with pipewire.
