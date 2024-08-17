7409b47fc490a642eea385eeae34149c66b96932
Baptiste Hardy <baptiste@hardy.sh>
2024-08-17 12:01:56 +0200

    Moved some packages from system module to dev-tools

--- home/dev-tools.nix
+++ home/dev-tools.nix
@@ -2,16 +2,31 @@
 
 {
   home.packages = with pkgs; [
+    # Visual git client
+    lazygit
+
     # API testing client
     httpie
+
+    # JSON viewer
+    fx
+
+    # JSON query language CLI tool
+    jql
+
+    # Selector for data structures
+    dasel
 
     # S3 client
     minio-client
 
+    # Docker management TUI
+    # lazydocker
+
+    # K8 dashboard TUI
+    # kdash
+
     # load testing tool
     k6
-
-    # coding exercices
-    exercism
   ];
 }
