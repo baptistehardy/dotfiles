e428c317466ac67f4f4e537e2623339464ee8c54
Baptiste Hardy <baptiste@hardy.sh>
2024-08-22 13:42:48 +0200

    Added stable Rust in flake

--- flake.nix
+++ flake.nix
@@ -12,13 +12,19 @@
     nvchad4nix = {
       url = "github:NvChad/nix";
       inputs.nixpkgs.follows = "nixpkgs";
-    };   
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
     home-manager,
+    rust-overlay,
     ...
   }:
   {
@@ -28,16 +34,21 @@
 
         modules = [
           ./hosts/tachyon
+          
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [ rust-overlay.overlays.default ];
+            environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
+          })          
 
           home-manager.nixosModules.home-manager {
             home-manager = {
-              extraSpecialArgs = { inherit inputs; };
+              extraSpecialArgs = { inherit inputs nixpkgs; };
               useGlobalPkgs = true;
               useUserPackages = true;
               users.baptiste = import ./home;
             };
           }
-        ];
+        ];        
 
         specialArgs = { inherit inputs; };
       };
